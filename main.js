9b5c625d128486d5f3a5be2c7a9a03fc27ea170f
mallehcar99 <b021910105@student.utem.edu.my>
2021-03-31 11:40:49 +0800

    connect to mongo

---
+++ main.js
@@ -0,0 +1,23 @@
+const {MongoClient} = require('mongodb');
+async function main() {
+    const uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.h3osg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    const client = new MongoClient(uri);
+    
+    try {
+        await client.connect();
+        await listDatabases(client);
+    } catch (e) {
+        console.error(e);
+    } finally {
+        await client.close();
+    }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+    databasesList = await client.db().admin().listDatabases();
+
+    console.log("Databases:");
+    databasesList.databases.forEach(db => console.log(` -${db.name}`));
+};
